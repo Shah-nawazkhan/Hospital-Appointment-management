0cb35f7e7724c4ec9981dba09471e9cf82979bbc
usmansami7861 <usmansami7861@gmail.com>
2023-09-27 01:21:23 +0500

    add to git

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'phone' => '+92 123432325',
+            'address' => 'usa',
+            'usertype'=> '1',
+        ]);
+    }
+}
